06c64d20067bb92cfc8ad27764ca3051e62d00e7
adhirajChauhan <adhirajsingh0105@gmail.com>
2024-09-05 20:20:31 +0530

    05092024

--- LPU_CPP_DSA_AUG24/DSA/02_LinkedList/01.cpp
+++ LPU_CPP_DSA_AUG24/DSA/02_LinkedList/01.cpp
@@ -12,8 +12,82 @@ class Node{
         next = NULL;
     }
 };
+
+void insertAtTail(Node* &head, int val){
+    //create a new node in which data feild will have val and next pointer will be NULL
+    Node* n = new Node(val);
+
+    if(head == NULL){
+        head = n;
+        return;
+    }
+
+    Node* temp = head;
+
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+
+    temp->next = n;
+
+
+}
+
+void insertAtHead(Node* &head, int val){
+
+    Node* n = new Node(val);
+
+    n->next = head;
+    head = n;
+}
+
+void deletion(Node* &head, int val){
+    Node* temp = head;
+
+    if(head == NULL) return;
+
+    while(temp->next->data != val){
+        temp= temp->next;
+    }
+
+    Node* toDelete = temp->next;
+    temp->next = temp->next->next;
+    delete toDelete;
+
+}
+
+void deletionAtHead(Node* &head){
+    Node* toDelete = head;
+    head = head->next;
+    delete toDelete;
+}
+
+void display(Node* head){
+
+    Node* temp = head;
+
+    while(temp!=NULL){
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+
+    
+}
  
 int main()
 {
     Node* head = NULL;
+
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+
+    insertAtHead(head, 9);
+    insertAtHead(head, 100);
+
+    deletion(head, 2);
+
+
+    display(head);
 }
