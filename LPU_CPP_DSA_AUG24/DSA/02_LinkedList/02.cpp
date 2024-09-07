5683d67bb2103bee6c21896cce52c79603847a3c
adhirajChauhan <adhirajsingh0105@gmail.com>
2024-09-07 12:30:08 +0530

    07092024

--- LPU_CPP_DSA_AUG24/DSA/02_LinkedList/02.cpp
+++ LPU_CPP_DSA_AUG24/DSA/02_LinkedList/02.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 class Node{
+    public:
     int data;
     Node* prev;
     Node* next;
@@ -15,17 +16,89 @@ class Node{
     }
 };
 
+void insertAtHead(Node* &head, int val){
+    Node* n = new Node(val);
+
+    n->next = head;
+
+    if(head != NULL){
+        head->prev = n;
+    }
+    head = n;
+}
+
 void insertAtTail(Node* &head, int val){
 
+    Node* n = new Node(val);
+    Node* temp = head;
+
+    if(head == NULL){
+        insertAtHead(head, val);
+        return;
+    }
+
+    while(temp->next != NULL){
+        temp=temp->next;
+    }
+    temp->next = n;
+    n->prev = temp;
+
 }
 
-void insertAtHead(){
+void deleteAtHead(Node* &head){
+    Node* temp = head;
+    head = head->next;
+    head->prev = NULL;
+
+    delete temp;
+}
+
+void deletion(Node* &head, int pos){
+
+    Node* temp = head;
+    int count = 1;
+
+    if(pos == 1){
+        deleteAtHead(head);
+        return;
+    }
+
+    while(temp!= NULL && count != pos){
+        temp = temp->next;
+        count++;
+    }
+
+    temp->prev->next = temp->next;
+    if(temp->next != NULL){
+        temp->next->prev = temp->prev;
+    }
+
+    delete temp;
 
 }
 
-//deletion
+void display(Node* head){
+    Node* temp = head;
+
+    while(temp!= NULL){
+        cout << temp->data << " -> ";
+        temp= temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+
  
 int main()
 {
- 
+    Node* head = NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+
+    insertAtHead(head, 10);
+
+    deletion(head, 4);
+
+    display(head);
 }
---
+++ LPU_CPP_DSA_AUG24/DSA/02_LinkedList/03.cpp
@@ -0,0 +1,19 @@
+//circular ll
+
+#include<iostream>
+using namespace std;
+ 
+class Node{
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+}
+
+int main()
+{
+ 
+}
